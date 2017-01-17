14b98ca63b5b037ceb25fa1b22870909a23b821a
Vlad Verestiuc <verestiuc.vlad@gmail.com>
2017-01-17 13:04:10 +0200

    Documentation and some cleanup

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/vvlad/evediscordkm"
 )
@@ -9,6 +11,12 @@ import (
 func main() {
 	configFile := flag.String("config", "", "configuration file path")
 	flag.Parse()
+
+	if _, err := os.Stat(*configFile); err != nil {
+		fmt.Println("a config file is required and must be a valid file")
+		os.Exit(1)
+	}
+
 	var config = evediscordkm.ReadFile(*configFile)
 	var zKillBoard = evediscordkm.NewZKillboard(config)
 	var reportProcessor = evediscordkm.NewReportProcessor(config)
